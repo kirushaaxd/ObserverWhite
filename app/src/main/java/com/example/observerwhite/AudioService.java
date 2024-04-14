216bc2a921cdc5ef7539f5afaceb33f72380dff8
Kirill <125600107+kirushaaxd@users.noreply.github.com>
2024-04-14 16:53:27 +0300

    Audio service

---
+++ app/src/main/java/com/example/observerwhite/AudioService.java
@@ -0,0 +1,36 @@
+package com.example.observerwhite;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class AudioService extends Service{
+    MediaPlayer player;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void onCreate() {
+        player = MediaPlayer.create(this, R.raw.music); //select music file
+        player.setLooping(true); //set looping
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return Service.START_NOT_STICKY;
+    }
+
+    public void onStopCommand(Intent intent, int flags){
+        player.stop();
+    }
+
+    public void onDestroy() {
+        player.stop();
+        player.release();
+        stopSelf();
+        super.onDestroy();
+    }
+}
